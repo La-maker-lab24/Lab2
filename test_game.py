6c5e91403ce93ac5802bad1bd5f83bea276db44f
La <lada_dementeva1@mail.ru>
2024-11-15 13:14:00 +0300

    new version

--- test_game.py
+++ test_game.py
@@ -6,6 +6,7 @@ class TestGame(unittest.TestCase):
 
     def setUp(self):
         self.game = Game()
+        self.stats = GameStatistics()
 
     def test_initial_scores(self):
         self.assertEqual(self.game.user_score, 0)
@@ -15,6 +16,30 @@ class TestGame(unittest.TestCase):
         result, computer_choice = self.game.play_round('rock')
         self.assertIn(computer_choice, self.game.choices)
         self.assertIn(result, ['user', 'computer', 'draw'])
+        result, computer_choice = self.game.play_round('paper')
+        self.assertIn(computer_choice, self.game.choices)
+        self.assertIn(result, ['user', 'computer', 'draw'])
+        result, computer_choice = self.game.play_round('rock')
+        self.assertIn(computer_choice, self.game.choices)
+        self.assertIn(result, ['user', 'computer', 'draw'])
+
+    def test_computer_choice_based_on_last_user_choice(self):
+        self.game.play_round('rock')
+        self.game.play_round('rock')
+        computer_choice = self.game.get_computer_choice()
+        self.assertEqual(computer_choice, 'paper')
+
+        self.game.play_round('paper')
+        self.game.play_round('paper')
+        self.game.play_round('paper')
+        computer_choice = self.game.get_computer_choice()
+        self.assertEqual(computer_choice, 'scissors')
+
+        self.game.play_round('scissors')
+        self.game.play_round('scissors')
+        self.game.play_round('scissors')
+        computer_choice = self.game.get_computer_choice()
+        self.assertEqual(computer_choice, 'rock')
 
     def test_determine_winner(self):
         self.assertEqual(self.game._determine_winner('rock', 'scissors'), 'user')
@@ -33,12 +58,6 @@ class TestGame(unittest.TestCase):
         self.game.reset_game()
         self.assertFalse(self.game.is_game_over())
 
-
-class TestGameIntegration(unittest.TestCase):
-
-    def setUp(self):
-        self.game = Game()
-
     def test_full_game_flow(self):
         self.game.play_round('rock')
         self.game.play_round('paper')
@@ -49,27 +68,34 @@ class TestGameIntegration(unittest.TestCase):
         self.game.user_score = 8
         self.assertTrue(self.game.is_game_over())
 
-
-class TestGameStatisticsIntegration(unittest.TestCase):
-
-    def setUp(self):
-        self.game = Game()
-        self.stats = GameStatistics()
-
-    def test_record_game_statistics(self):
-        for _ in range(5):
-            result, _ = self.game.play_round('rock')
-            self.stats.record_game(result)
-
-        self.assertEqual(self.stats.games_played, 5)
-        self.assertTrue(self.stats.user_wins + self.stats.computer_wins <= 5)
-
     def test_reset_statistics(self):
         self.stats.record_game('user')
         self.stats.reset_statistics()
         self.assertEqual(self.stats.games_played, 0)
         self.assertEqual(self.stats.user_wins, 0)
         self.assertEqual(self.stats.computer_wins, 0)
+
+    def test_computer_choice_when_no_previous_choices(self):
+        computer_choice = self.game.get_computer_choice()
+        self.assertIn(computer_choice, self.game.choices)
+        self.game.play_round('rock')
+        computer_choice = self.game.get_computer_choice()
+        self.assertIn(computer_choice, self.game.choices)
+
+    def test_get_statistics_initial(self):
+        statistics = self.stats.get_statistics()
+        self.assertEqual(statistics['games_played'], 0)
+        self.assertEqual(statistics['user_wins'], 0)
+        self.assertEqual(statistics['computer_wins'], 0)
+
+    def test_record_game_statistics(self):
+        self.stats.record_game('user')
+        self.stats.record_game('computer')
+        self.stats.record_game('user')
+        statistics = self.stats.get_statistics()
+        self.assertEqual(statistics['games_played'], 3)
+        self.assertEqual(statistics['user_wins'], 2)
+        self.assertEqual(statistics['computer_wins'], 1)
 
 
 if __name__ == '__main__':
